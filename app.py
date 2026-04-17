3d87a069e7a7193ef2c71658ac2e2fb3775540e8
Phan Tôn Lộc Nguyên <156264501+ptlocnguyen@users.noreply.github.com>
2026-04-18 06:23:52 +0700

    Integrate Databricks for face recognition storage

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 import numpy as np
+from databricks import sql
 
 app = Flask(__name__)
+CORS(app)
 
-# ===== DATABASE giả lập (sau thay Databricks)
-faces_db = []
+# ===== CONFIG DATABRICKS =====
+server_hostname = "YOUR_HOST"
+http_path = "YOUR_HTTP_PATH"
+access_token = "YOUR_TOKEN"
 
-# ===== AI CALL =====
+# ===== CONNECT =====
+def get_conn():
+    return sql.connect(
+        server_hostname=server_hostname,
+        http_path=http_path,
+        access_token=access_token
+    )
+
+# ===== AI =====
 def get_embedding(image_bytes):
     url = "https://bufalo-api-973102760389.asia-southeast1.run.app/predict"
 
@@ -20,16 +33,13 @@ def get_embedding(image_bytes):
     if res.status_code != 200:
         return None
 
-    data = res.json()
-    return data.get("embedding")
-
+    return res.json().get("embedding")
 
 # ===== COSINE =====
 def cosine(a, b):
     a = np.array(a)
     b = np.array(b)
     return np.dot(a,b)/(np.linalg.norm(a)*np.linalg.norm(b))
-
 
 # ===== REGISTER =====
 @app.route("/register", methods=["POST"])
@@ -43,13 +53,18 @@ def register():
     if emb is None:
         return "No face", 400
 
-    faces_db.append({
-        "name": name,
-        "embedding": emb
-    })
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO face_db.faces VALUES (?, ?)",
+        (name, emb)
+    )
+
+    cursor.close()
+    conn.close()
 
     return "Saved"
-
 
 # ===== RECOGNIZE =====
 @app.route("/recognize", methods=["POST"])
@@ -57,23 +72,29 @@ def recognize():
 
     emb = request.json.get("embedding")
 
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name, embedding FROM face_db.faces")
+
     best_name = "Unknown"
     best_score = 0
 
-    for f in faces_db:
+    for row in cursor.fetchall():
 
-        score = cosine(emb, f["embedding"])
+        name = row[0]
+        emb_db = row[1]
+
+        score = cosine(emb, emb_db)
 
         if score > best_score:
             best_score = score
-            best_name = f["name"]
+            best_name = name
+
+    cursor.close()
+    conn.close()
 
     if best_score > 0.5:
         return jsonify({"name": best_name})
 
     return jsonify({"name": "Unknown"})
-
-
-# ===== RUN =====
-if __name__ == "__main__":
-    app.run()
